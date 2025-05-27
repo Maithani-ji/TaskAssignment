ee8590f0896ddf7039f9ff5cf5425e5cb9edde96
Your Name <maithaniji.official@gmail.com>
2025-05-27 21:50:05 +0530

    working in phase 2 , transactions and aggregation things left

--- backend/src/controllers/userPrismaController.js
+++ backend/src/controllers/userPrismaController.js
@@ -75,3 +75,106 @@ export const createTask=async(req,res,next)=>{
         next(err)
     }
 }
+export const getTasks=async(req,res,next)=>{
+    logger.info("Get tasks process started")
+    try {
+        const {userId}=req.query 
+        const tasks= await prisma.task.findMany(
+            {
+                where:{userId:Number(userId)},
+                select:{
+                    title:true,
+                    description:true,
+                    dueDate:true,
+                    id:true,
+                    
+
+
+                    user:{
+                        select:{
+                            name:true,
+                            id:true,
+                        },
+                    },
+                },//for the tasks
+
+                // use above or below , both select and include wont work together
+               
+                // include:{user:false},
+
+                // include:{
+                //     user:{
+                //         select:{
+                //             name:true,
+                //         },
+                //     }},
+                
+            },
+        )
+        res.success(200,"Tasks fetched successfully",tasks)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const getTaskPaginated=async(req,res,next)=>{
+    try {
+        logger.info("Get task paginated process started")
+        const {userId,limit=3,page=1,search,status}=req.query
+        const filter={
+            userId:Number(userId),
+            dueDate:{
+                gte:new Date(),
+            },
+            ...(status && {status}),
+            ...(search && {
+                OR:[
+                    {title:{contains:search ,mode:"insensitive"}},
+                    {description:{contains:search,mode:"insensitive"}},
+                ],
+            }),
+        }
+        const tasks= await prisma.task.findMany(
+            {
+                where:filter,
+                skip:(page-1)*Number(limit),
+                take:Number(limit),
+                select:{
+                    title:true,
+                    description:true,
+                    dueDate:true,
+                    id:true,
+                    status:true,
+                    user:{
+                        select:{
+                            name:true,
+                            id:true,
+                        },
+                    },
+                },
+                orderBy:{
+                    createdAt:"asc",
+                },
+
+            },
+        )
+       
+        const total= await prisma.task.count({
+            where:filter,
+        })
+        const totalPages= Math.ceil(total/limit)
+        res.success(200,"Tasks fetched successfully",{
+            tasks,
+            pagination:{
+                currentPage:page,
+                pageSize:limit,
+                totalPages,
+                totalData:total,
+                hasNextPage:page<totalPages,
+                hasPrevPage:page>1,
+            },
+        })
+        } catch (error) {
+        next(error)
+    }
+}
--- backend/src/routes/userSqlRoutes.js
+++ backend/src/routes/userSqlRoutes.js
@@ -1,10 +1,12 @@
 import express from "express"
-import { createTask, login, signup, updateUser } from "../controllers/userPrismaController.js"
+import { createTask, getTaskPaginated, getTasks, login, signup, updateUser } from "../controllers/userPrismaController.js"
 import { validateHandlerJoi } from "../middilewares/validateHandlerJoi.js"
 import { loginValidationRules } from "../validators/userSqlValidator.js"
 
 const userSqlRoute=express.Router()
 
+userSqlRoute.get("/get-users-task-sql",getTasks)
+userSqlRoute.get("/get-tasks-paginate-sql",getTaskPaginated)
 userSqlRoute.post("/sign-up-user-sql",signup)
 userSqlRoute.post("/log-in-user-sql",validateHandlerJoi(loginValidationRules()),login)
 userSqlRoute.put("/update-user-sql/:id",updateUser)
